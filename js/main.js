d5a604cb78d6b531faf2652b262493b153d32448
erinborgmeyer <borg0027@algonquinlive.com>
2016-03-11 17:30:25 -0500

    fixing details

--- js/main.js
+++ js/main.js
@@ -1,35 +1,46 @@
-var $header = $('header');
+var 
+$header = $('header');
 $header.addClass('masthead');
 
-var $h1 = $('h1');
+var 
+$h1 = $('h1');
 $h1.removeClass('big-heading');
 $h1.addClass('logo');
 
-var $main = $('main');
+var 
+$main = $('main');
 $main.addClass('wrapper');
 
-var $p = $('p:first-child');
+var 
+$p = $('p:first-child');
 $p.addClass('intro');
 
-var $h2 = $('h2');
+var 
+$h2 = $('h2');
 $h2.addClass('slug-head');
 
-var $list = $('main ul');
+var 
+$list = $('main ul');
 $list.addClass('slug-list');
 
-var $main = $('images');
+var 
+$main = $('images');
 $main.addClass('slug-img');
 
-var $liFirst = $('li:first-child');
+var 
+$liFirst = $('li:first-child');
 $liFirst.addClass('slug-list-first');
 
-var $li = $('li:last-child');
+var 
+$li = $('li:last-child');
 $li.addClass('slug-list-last');
 
-var $dl = $('dl');
+var 
+$dl = $('dl');
 $dl.addClass('classification');
 
-var $dt = $('dt');
+var
+$dt = $('dt');
 $dt.removeClass('big-label');
 
 $li.show();
