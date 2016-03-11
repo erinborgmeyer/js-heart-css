8276bcc872290ea3eabb37cb4a3ccf50cbc6ae01
erinborgmeyer <borg0027@algonquinlive.com>
2016-03-11 17:28:27 -0500

    done

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var $header = $('header');
+$header.addClass('masthead');
+
+var $h1 = $('h1');
+$h1.removeClass('big-heading');
+$h1.addClass('logo');
+
+var $main = $('main');
+$main.addClass('wrapper');
+
+var $p = $('p:first-child');
+$p.addClass('intro');
+
+var $h2 = $('h2');
+$h2.addClass('slug-head');
+
+var $list = $('main ul');
+$list.addClass('slug-list');
+
+var $main = $('images');
+$main.addClass('slug-img');
+
+var $liFirst = $('li:first-child');
+$liFirst.addClass('slug-list-first');
+
+var $li = $('li:last-child');
+$li.addClass('slug-list-last');
+
+var $dl = $('dl');
+$dl.addClass('classification');
+
+var $dt = $('dt');
+$dt.removeClass('big-label');
+
+$li.show();
+
+
+
